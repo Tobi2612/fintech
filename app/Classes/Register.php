b76852197881134602d0c57db682a1a9196898aa
Tobi <tobisaid007@gmail.com>
2021-12-13 21:28:24 +0100

    username and email validations

--- app/Classes/Register.php
+++ app/Classes/Register.php
@@ -8,74 +8,114 @@ use Illuminate\Http\Request;
 use App\Models\Account;
 use App\Models\User;
 
-class Register {
- 
-    private function generate_account_number($userr){
-        $dat['account_number'] = 1000000000 + $userr->id;
+class Register
+{
+
+    private function generate_account_number($new_user)
+    {
+        $dat['account_number'] = 1000000000 + $new_user->id;
         $dat['account_balance'] = 5000000;
-        $dat['user_id'] = $userr->id;
-        $dat['first_name'] = $userr->first_name;
-        $dat['pin'] = $userr->pin;
-        $dat['username'] = $userr->username;
-        $dat['email'] = $userr->email;
+        $dat['user_id'] = $new_user->id;
+        $dat['first_name'] = $new_user->first_name;
+        $dat['pin'] = $new_user->pin;
+        $dat['username'] = $new_user->username;
+        $dat['email'] = $new_user->email;
 
-        if($userr->account_type === 'user'){
+        if ($new_user->account_type === 'user') {
             $dat['account_type'] = 'user';
-        }
-
-        else{
+        } else {
             $dat['account_type'] = 'admin';
         }
-    
+
         return Account::create($dat);
     }
 
 
 
-    public function create(RegisterRequest $request){
-        
-        $data = $request -> validated();
-        $no = $request->username;
-        $user = json_decode(User::all(),true);
-        $filtered = array_filter($user , function($u) use($no) {
-            return $u['username'] === $no;
+    public function create(RegisterRequest $request)
+    {
+
+        $data = $request->validated();
+        $check_username = $request->username;
+        $user = json_decode(User::all(), true);
+        $filtered = array_filter($user, function ($u) use ($check_username) {
+            return strtolower($u['username']) === strtolower($check_username);
         });
 
-        if(count($filtered)>0){
-            return "Username already exists";
+
+        //check for white spaces in username
+
+        if (!preg_match('/^\S*$/', $check_username)) {
+            return response()->json([
+                'success' => False,
+                'error' => 'Incorrect Username format'
+            ], 400);
         }
 
-        else{
-        $data['password'] = bcrypt($data['password']);
-        $data['account_type'] = 'user';
-        $data['pin'] = bcrypt($data['pin']);
-        $userr = User::create($data);
-        $this->generate_account_number($userr);
-        return $userr;
+        // Must start with letter
+        // 1-20 characters
+        // Letters and numbers only
+        if (!preg_match('/^[A-Za-z]{1}[A-Za-z0-9]{0,20}$/', $check_username)) {
+            return response()->json([
+                'success' => False,
+                'error' => 'Incorrect Username format'
+            ], 400);
+        }
+
+        if (count($filtered) > 0) {
+            return "Username already exists";
+        } else {
+            $data['password'] = bcrypt($data['password']);
+            $data['account_type'] = 'user';
+            $data['email'] = strtolower($data['email']);
+            $data['pin'] = bcrypt($data['pin']);
+            $new_user = User::create($data);
+            $this->generate_account_number($new_user);
+            return $new_user;
         }
     }
 
 
-    public function createAdmin(CreateAdminRequest $request){
-        $data = $request -> validated();
-        $no = $request->username;
-        $user = json_decode(User::all(),true);
-        $filtered = array_filter($user , function($u) use($no) {
-            return $u['username'] === $no;
+    public function createAdmin(CreateAdminRequest $request)
+    {
+        $data = $request->validated();
+        $check_username = $request->username;
+        $user = json_decode(User::all(), true);
+        $filtered = array_filter($user, function ($u) use ($check_username) {
+            return strtolower($u['username']) === strtolower($check_username);
         });
 
-        if(count($filtered)>0){
+        //check for white spaces in username
+
+        if (!preg_match('/^\S*$/', $check_username)) {
+            return response()->json([
+                'success' => False,
+                'error' => 'Incorrect Username format'
+            ], 400);
+        }
+
+        // Must start with letter
+        // 1-20 characters
+        // Letters and numbers only
+        if (!preg_match('/^[A-Za-z]{1}[A-Za-z0-9]{0,20}$/', $check_username)) {
+            return response()->json([
+                'success' => False,
+                'error' => 'Incorrect Username format'
+            ], 400);
+        }
+
+
+
+        if (count($filtered) > 0) {
             return "Username already exists";
+        } else {
+            $data['password'] = bcrypt($data['password']);
+            $data['email'] = strtolower($data['email']);
+            $data['account_type'] = 'admin';
+            $data['pin'] = bcrypt($data['pin']);
+            $new_user = User::create($data);
+            $this->generate_account_number($new_user);
+            return $new_user;
         }
-
-        else{
-        $data['password'] = bcrypt($data['password']);
-        $data['account_type'] = 'admin';
-        $data['pin'] = bcrypt($data['pin']);
-        $userr = User::create($data);
-        $this->generate_account_number($userr);
-        return $userr;
-        }
-    } 
-
+    }
 }
